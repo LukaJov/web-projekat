1eb4a8cb6453c6cf4ebae8b92fad72b8745b82f6
Luka <jovanovic.luka1717@gmail.com>
2021-05-25 21:06:03 +0200

    Added UserService and FitnessCenterService

---
+++ src/main/java/service/FitnessCenterService.java
@@ -0,0 +1,21 @@
+package service;
+import java.util.Date;
+import java.util.List;
+
+import com.example.demo.model.FitnessCenter;
+import com.example.demo.repository.FitnessCenterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+public class FitnessCenterService {
+
+    @Autowired
+    private FitnessCenterRepository fitnessCenterRepository;
+
+    public FitnessCenter save(FitnessCenter fitnessCenter)
+    {
+        return this.fitnessCenterRepository.save(fitnessCenter);
+    }
+
+
+}
--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,4 +1,19 @@
 package service;
 
+import java.util.Date;
+import java.util.List;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User save(User user)
+    {
+        return this.userRepository.save(user);
+    }
 }
