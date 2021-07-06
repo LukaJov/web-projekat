fc0361c8493dd3e59a8ecee0d4d880de86380cbf
Luka <jovanovic.luka1717@gmail.com>
2021-07-06 20:31:45 +0200

    Added term creation in term service

--- src/main/java/com/example/demo/service/TermService.java
+++ src/main/java/com/example/demo/service/TermService.java
@@ -5,10 +5,8 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
-import com.example.demo.model.Term;
-import com.example.demo.model.Trainer;
-import com.example.demo.model.User;
-import com.example.demo.repository.TermRepository;
+import com.example.demo.model.*;
+import com.example.demo.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
@@ -20,31 +18,35 @@ public class TermService {
     @Autowired
     private TermRepository termRepository;
 
-    private Sort.Order getSorting(String sort)
-    {
-        if(sort.equals("price,asc"))
-        {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private FitnessCenterRepository fitnessCenterRepository;
+
+    @Autowired
+    private RoomRepository roomRepository;
+
+    @Autowired
+    private TrainerRepository trainerRepository;
+
+    private Sort.Order getSorting(String sort) {
+        if (sort.equals("price,asc")) {
             List<Sort.Order> orders = new ArrayList<>();
             Sort.Order order = new Sort.Order(Sort.Direction.ASC, "price");
             orders.add(order);
             return order;
-        }
-        else if(sort.equals("price,desc"))
-        {
+        } else if (sort.equals("price,desc")) {
             List<Sort.Order> orders = new ArrayList<>();
             Sort.Order order = new Sort.Order(Sort.Direction.DESC, "price");
             orders.add(order);
             return order;
-        }
-        else if(sort.equals("date,asc"))
-        {
+        } else if (sort.equals("date,asc")) {
             List<Sort.Order> orders = new ArrayList<>();
             Sort.Order order = new Sort.Order(Sort.Direction.ASC, "date");
             orders.add(order);
             return order;
-        }
-        else
-        {
+        } else {
             List<Sort.Order> orders = new ArrayList<>();
             Sort.Order order = new Sort.Order(Sort.Direction.DESC, "date");
             orders.add(order);
@@ -53,42 +55,37 @@ public class TermService {
 
     }
 
-    public List<Term> findAll(String sort)
-    {
+    public List<Term> findAll(String sort) {
         Sort.Order order = getSorting(sort);
         return this.termRepository.findAll(Sort.by(order));
     }
-    public Optional<Term> findById(Long id)
-    {
+
+    public Optional<Term> findById(Long id) {
         return this.termRepository.findById(id);
     }
-    public List<Term> findByTrainingName(String name, String sort)
-    {
+
+    public List<Term> findByTrainingName(String name, String sort) {
         Sort.Order order = getSorting(sort);
-        return this.termRepository.findByTrainingNameContaining(name,Sort.by(order) );
+        return this.termRepository.findByTrainingNameContaining(name, Sort.by(order));
     }
 
-    public List<Term> findByTrainingDesc(String desc, String sort)
-    {
+    public List<Term> findByTrainingDesc(String desc, String sort) {
         Sort.Order order = getSorting(sort);
-        return this.termRepository.findByTrainingDescContaining(desc,Sort.by(order) );
+        return this.termRepository.findByTrainingDescContaining(desc, Sort.by(order));
     }
     //findbytrainingtype probaj
 
-    public List<Term> findByTrainingTrainingType(String trainingType, String sort)
-    {
+    public List<Term> findByTrainingTrainingType(String trainingType, String sort) {
         Sort.Order order = getSorting(sort);
-        return this.termRepository.findByTrainingTrainingTypeContaining(trainingType,Sort.by(order));
+        return this.termRepository.findByTrainingTrainingTypeContaining(trainingType, Sort.by(order));
     }
 
-    public List<Term> findByDate(Date date, String sort)
-    {
+    public List<Term> findByDate(Date date, String sort) {
         Sort.Order order = getSorting(sort);
         return this.termRepository.findByDateIsBefore(date, Sort.by(order));
     }
 
-    public List<Term> findByPrice(Double price, String sort)
-    {
+    public List<Term> findByPrice(Double price, String sort) {
         Sort.Order order = getSorting(sort);
         return this.termRepository.findByPriceIsLessThanEqual(price, Sort.by(order));
     }
@@ -98,20 +95,37 @@ public class TermService {
         return this.termRepository.findByUserId(id);
     }*/
 
-    public Term signup(Term term, User user)
-    {
+    public Term signup(Term term, User user) {
         term.getUserToDo().add(user);
-        term.setNumberOfUsers(term.getNumberOfUsers()+1);
+        user.getToDo().add(term);
+        term.setNumberOfUsers(term.getNumberOfUsers() + 1);
+        this.userRepository.save(user);
         Term savedTerm = this.termRepository.save(term);
         return savedTerm;
     }
 
-    public Term signout(Term term, User user)
-    {
+    public Term signout(Term term, User user) {
         term.getUserToDo().remove(user);
-        term.setNumberOfUsers(term.getNumberOfUsers()-1);
+        user.getToDo().remove(term);
+        term.setNumberOfUsers(term.getNumberOfUsers() - 1);
+        this.userRepository.save(user);
         Term savedTerm = this.termRepository.save(term);
         return savedTerm;
+    }
+
+    public Term save(Term term, FitnessCenter fitnessCenter, Room room, Trainer trainer)
+    {
+        term.setFitCenter(fitnessCenter);
+        term.setRoom(room);
+        term.setTrainer(trainer);
+        fitnessCenter.getTerms().add(term);
+        room.getTerms().add(term);
+        trainer.getTerms().add(term);
+        this.fitnessCenterRepository.save(fitnessCenter);
+        this.roomRepository.save(room);
+        this.trainerRepository.save(trainer);
+
+        return this.termRepository.save(term);
     }
 
 }
